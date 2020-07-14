129aa692721c24c46fbaaaa390215d6eb9ec33e2
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-14 00:30:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Add your code here
+class Dog 
+  @@all = []
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  def self.all 
+    @@all 
+  end
+  def self.print_all 
+    for i in 
+    
