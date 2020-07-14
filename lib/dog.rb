e9197857a15a6674015b3ee09a8f40e49e83329a
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-14 00:35:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,9 @@ class Dog
   attr_accessor :name 
   def initialize(name)
     @name = name 
-     
+    save    
   end 
-  def save
-    @@all << self 
-  end 
+  
   def self.all 
     @@all 
   end
